588ed1c7b63a812bfee91b1d4fbb91b887f9a34c
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-05-20 16:34:18 -0700

    whatever

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,89 @@
+import { useState } from "react";
 import "./App.css";
 import Content from "./posts/example.mdx";
 import Post from "./posts/jordan-baker.mdx";
 import Reading from "./posts/reading.mdx";
 
-function App() {
+function Info({
+  setDisplayInfo,
+}: {
+  setDisplayInfo: (displayInfo: boolean) => void;
+}) {
   return (
-    <main>
-      <div>
-        <h1 id="title">secret@shenaichan ⋆.˚ ☾ .⭒˚ </h1>
-        <p>about</p>
-        <p>22 / they / queer</p>
+    <div id="infoPopup">
+      <p>
+        my name is shenai and this is my semi-secret personal microblogging
+        platform.
+      </p>
+      <p>there are two main feeds:</p>
+      <ol>
+        <li>
+          things i've written, usually 1-4 paragraph length posts about personal
+          things, media i'm thinking about, &c.
+        </li>
+        <li>
+          things other people have made; various flavors of inspiration. these
+          are yoinked from my single are.na board where i put nearly everything
+          that interests me (e.g. blog posts, art, books)
+        </li>
+      </ol>
+      <p>
+        the answer to "why did you make that design decision" is invariably: i
+        spent a lot of time on tumblr growing up :)
+      </p>
+      <p>thank you for coming and enjoy your stay xoxoxo {"<3"}</p>
+      <button onClick={() => setDisplayInfo(false)}>close</button>
+    </div>
+  );
+}
 
-        <p>what is this?</p>
-        <p>i spent a lot of time on tumblr growing up </p>
-      </div>
-      <div className="readerContainer">
-        <p>by me</p>
-        <div className="reader">
-          <div className="post">
-            <div className="time">
-              <p>2025.05.20</p>
-              <p>00:34</p>
+function App() {
+  const [displayInfo, setDisplayInfo] = useState(false);
+  return (
+    <>
+      {displayInfo && <Info setDisplayInfo={setDisplayInfo} />}
+      <main>
+        <div id="sidebar">
+          <br />
+          <h1 id="title">secret@shenaichan</h1>
+          <h1> ⋆.˚ ☾ .⭒˚ </h1>
+
+          <br />
+
+          <button onClick={() => setDisplayInfo(true)}>what is this?</button>
+        </div>
+        <div className="readerContainer">
+          <p>by me</p>
+          <div className="reader">
+            <div className="post">
+              <div className="time">
+                <p>2025.05.20</p>
+                <p>00:34</p>
+              </div>
+              <Content />
             </div>
-            <Content />
-          </div>
-          <div className="post">
-            <div className="time">
-              <p>2025.05.20</p>
-              <p>00:34</p>
+            <div className="post">
+              <div className="time">
+                <p>2025.05.20</p>
+                <p>00:34</p>
+              </div>
+              <Post />
             </div>
-            <Post />
-          </div>
-          <div className="post">
-            <div className="time">
-              <p>2025.05.20</p>
-              <p>00:34</p>
+            <div className="post">
+              <div className="time">
+                <p>2025.05.20</p>
+                <p>00:34</p>
+              </div>
+              <Reading />
             </div>
-            <Reading />
           </div>
         </div>
-      </div>
-      <div className="readerContainer">
-        <p>by others</p>
-        <div className="reader"></div>
-      </div>
-    </main>
+        <div className="readerContainer">
+          <p>by others</p>
+          <div className="reader"></div>
+        </div>
+      </main>
+    </>
   );
 }
 
