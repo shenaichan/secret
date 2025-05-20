260666f8ea1d2dfec6da78af54cb45be07b0a7d9
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-05-20 15:24:07 -0700

    anyway

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Content from "./posts/example.mdx";
+import Post from "./posts/jordan-baker.mdx";
+import Reading from "./posts/reading.mdx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <main>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 id="title">secret@shenaichan ⋆.˚ ☾ .⭒˚ </h1>
+        <p>about</p>
+        <p>22 / they / queer</p>
+
+        <p>what is this?</p>
+        <p>i spent a lot of time on tumblr growing up </p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="readerContainer">
+        <p>by me</p>
+        <div className="reader">
+          <div className="post">
+            <div className="time">
+              <p>2025.05.20</p>
+              <p>00:34</p>
+            </div>
+            <Content />
+          </div>
+          <div className="post">
+            <div className="time">
+              <p>2025.05.20</p>
+              <p>00:34</p>
+            </div>
+            <Post />
+          </div>
+          <div className="post">
+            <div className="time">
+              <p>2025.05.20</p>
+              <p>00:34</p>
+            </div>
+            <Reading />
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="readerContainer">
+        <p>by others</p>
+        <div className="reader"></div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
