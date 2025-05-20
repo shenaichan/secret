0fe609db075bab680d62cff7e72464f5717d86e1
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-05-20 15:25:48 -0700

    fixed build err

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Content from "./posts/example.mdx";
 import Post from "./posts/jordan-baker.mdx";
 import Reading from "./posts/reading.mdx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main>
       <div>
